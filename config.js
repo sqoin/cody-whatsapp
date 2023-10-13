035ffbc1ad40916c5689ee122a6a980323f2ed47
aminovic <mohamedaminemnasri0@gmail.com>
2023-10-13 14:10:06 +0100

    change bot id

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const BASE_URL = 'https://getcody.ai/api/v1';
 const ACCESS_TOKEN = "";
+const BOT_ID  = "";
 
 module.exports = {
     BASE_URL,
-    ACCESS_TOKEN
+    ACCESS_TOKEN,
+    BOT_ID
 };
--- whatsapp.js
+++ whatsapp.js
@@ -1,6 +1,7 @@
 const { Client, Events, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require("qrcode");
 const { sendMessageToCody, fetchConversationsForBot, createConversation } = require('./api.js');
+const { BOT_ID } = require('./config.js');
 
 function initializeWhatsAppClient() {
     const client = new Client({
@@ -25,22 +26,23 @@ function initializeWhatsAppClient() {
         });
     });
 
-    //... [Keep other event listeners here.]
 
-    client.on(Events.MESSAGE_RECEIVED, async msg => {
+   /* client.on(Events.MESSAGE_RECEIVED, async msg => {
         console.log('Message received event triggered.');
         console.log(msg.body);
-
+        if ( ! msg.body) {
+            console.error(`empty body from ${msg.from} that won't be considered `);
+            return;
+        }
         if (msg.body !== '!ping') {  // Change this condition to suit when you want to send to Cody
-            console.log("not ping, I am here");
             // Step 1: Check if there's an existing conversation for that user
             const phoneNumber = msg.from; // This might need to be modified based on actual structure
-            const existingConversations = await fetchConversationsForBot("");  // Replace with your actual bot ID
+            const existingConversations = await fetchConversationsForBot("mWZdPjgDzaKg");  // Replace with your actual bot ID
             let conversationId = existingConversations.find(c => c.name === phoneNumber)?.id;
 
             // Step 2: If not, create a new conversation (you might need a create conversation function)
             if (!conversationId) {
-                conversationId = await createConversation("", phoneNumber);  // Assuming you have this function
+                conversationId = await createConversation("mWZdPjgDzaKg", phoneNumber);  // Assuming you have this function
             }
             console.log("conversation id : " + conversationId);
             // Step 3: Send the WhatsApp message to the Cody API
@@ -52,7 +54,63 @@ function initializeWhatsAppClient() {
             console.log('Ping command detected. Sending pong reply...');
             msg.reply('pong');
         }
-    });
+    }); */
+
+    let activeChats = {};
+
+client.on(Events.MESSAGE_RECEIVED, async msg => {
+    console.log('Message received event triggered.');
+    console.log(msg.body);
+    if (!msg.body) {
+        return;
+    }
+   /* if (!msg.body.startsWith("!")) {
+        return;
+    }*/
+    if (msg.body === '!ping') {
+        console.log('Ping command detected. Sending pong reply...');
+        msg.reply('pong');
+        return;
+    }
+
+    const phoneNumber = msg.from;
+
+    // Initialize or update the activeChats entry for the user
+    if (!activeChats[phoneNumber]) {
+        activeChats[phoneNumber] = {
+            messages: [],
+            timeout: null
+        };
+    }
+
+    activeChats[phoneNumber].messages.push(msg.body);
+
+    // Clear any previously set timeout
+    if (activeChats[phoneNumber].timeout) {
+        clearTimeout(activeChats[phoneNumber].timeout);
+    }
+
+    // Set a new timeout
+    activeChats[phoneNumber].timeout = setTimeout(async () => {
+        const concatenatedMsg = activeChats[phoneNumber].messages.join(' ');
+        
+        const botId = BOT_ID;
+       // const botId = "WJxboZXNkagw";
+        const existingConversations = await fetchConversationsForBot(botId); // Replace with your actual bot ID
+        let conversationId = existingConversations.find(c => c.name === phoneNumber)?.id;
+        
+        if (!conversationId) {
+            conversationId = await createConversation(botId, phoneNumber); // Assuming you have this function
+        }
+
+        const response = await sendMessageToCody(conversationId, concatenatedMsg);
+        msg.reply(response.data.content);
+
+        // Clear the entry for the user
+        delete activeChats[phoneNumber];
+
+    }, 5000);  // 5 seconds
+});
 
     return client;
 }
